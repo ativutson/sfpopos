3ae61ff9602d196a3341120ca4db75717084fd14
Ativut Son <nazareth_0@hotmail.com>
2021-11-18 13:43:22 -0500

    content component built

--- src/Title.js
+++ src/Title.js
@@ -5,9 +5,9 @@ function Title() {
     return (
         <div className="Title">
             <h1>SFPOPOS</h1>
-            <div className="Title-Subtitle">San Franciscos Privately Owned Public Spaces</div>
+                <div className="Title-Subtitle">San Franciscos Privately Owned Public Spaces</div>
         </div>
-    )
+    );
 };
 
 export default Title;
